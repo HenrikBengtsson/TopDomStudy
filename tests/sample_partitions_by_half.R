f921d073ba0f8090ff80d2ccd07f820357edfd52
Henrik Bengtsson <hb@aroma-project.org>
2018-11-27 12:36:36 -0800

    TESTS: Add more package tests validating the 'by_half' sampling methods

--- tests/sample_partitions_by_half.R
+++ tests/sample_partitions_by_half.R
@@ -1,32 +1,119 @@
 library(TopDomStudy)
 
 n <- 1000L
-parts <- sample_partitions(n, fraction = 0.5)
-str(parts)
-idxs <- sort(unlist(parts))
-stopifnot(identical(idxs, seq_len(n)))
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Equivalence to sample_partitions(fraction = 0.5)
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+set.seed(0xBEEF)
+parts0 <- sample_partitions(n, fraction = 0.5)
+str(parts0)
+idxs <- sort(unlist(parts0, use.names = FALSE))
+stopifnot(
+  length(parts0) == 2L,
+  length(idxs) == n,
+  identical(idxs, seq_len(n))
+)
 
 n <- 1000L
-parts <- sample_partitions_by_half(n, fraction = 0.2)
+set.seed(0xBEEF)
+parts <- sample_partitions_by_half(n, fraction = 0.5)
 str(parts)
-stopifnot(names(parts)[1] == "reference")
+idxs <- sort(unlist(parts, use.names = FALSE))
+stopifnot(
+  names(parts)[1] == "reference",
+  length(parts) == 2L,
+  length(idxs) == n,
+  identical(idxs, seq_len(n)),
+  all(parts[[1]] == parts0[[1]])
+)
 for (pp in 2:length(parts)) {
-  stopifnot(length(intersect(parts[[pp]], parts$reference)) == 0L)
+  stopifnot(length(
+    intersect(parts[[pp]], parts$reference)) == 0L,
+    length(parts[[pp]]) == length(parts$reference),
+    length(parts[[pp]]) == length(parts0[[pp]]),
+    all(sort(parts[[pp]]) == sort(parts0[[pp]]))  ## note sort()
+  )
 }
 
-set.seed(0x42)
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Equivalence to sample_partitions_similar_weights(fraction = 0.5)
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+set.seed(0xBEEF)
 n <- 1000L
 w <- runif(n)
-parts <- sample_partitions_similar_weights(w, fraction = 1/3)
+
+set.seed(0xBEEF)
+parts0 <- sample_partitions_similar_weights(w, fraction = 1/2)
+str(parts0)
+idxs <- sort(unlist(parts0, use.names = FALSE))
+stopifnot(
+  length(parts0) == 2L,
+  length(idxs) == n,
+  identical(idxs, seq_len(n))
+)
+
+set.seed(0xBEEF)
+parts <- sample_partitions_similar_weights_by_half(w, fraction = 1/2, w_tolerance = 0.01)
 str(parts)
-idxs <- sort(unlist(parts))
-stopifnot(identical(idxs, seq_len(n)))
+idxs <- sort(unlist(parts, use.names = FALSE))
+stopifnot(
+  names(parts)[1] == "reference",
+  length(parts) == 2L,
+  length(idxs) == n,
+  identical(idxs, seq_len(n)),
+  all(parts[[1]] == parts0[[1]])
+)
+for (pp in 2:length(parts)) {
+  stopifnot(length(
+    intersect(parts[[pp]], parts$reference)) == 0L,
+    length(parts[[pp]]) == length(parts$reference),
+    length(parts[[pp]]) == length(parts0[[pp]]),
+    all(sort(parts[[pp]]) == sort(parts0[[pp]]))  ## note sort()
+  )
+}
 
 
-set.seed(0x42)
-n <- 1000L
-w <- runif(n)
-parts <- sample_partitions_similar_weights_by_half(w, fraction = 0.4, w_tolerance = 0.01)
-str(parts)
-idxs <- sort(unlist(parts))
-#stopifnot(identical(idxs, seq_len(n)))
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Misc.
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+for (fraction in c(0.01, 0.2, 0.5)) {
+  set.seed(0xBEEF)
+  parts <- sample_partitions_by_half(n, fraction = fraction)
+  str(parts)
+  idxs <- sort(unlist(parts, use.names = FALSE))
+  stopifnot(
+    names(parts)[1] == "reference",
+    length(parts) == 2L,
+    all(parts[[1]] == parts0[[1]]),
+    length(idxs) == (0.5 + fraction) * n
+  )
+  for (pp in 2:length(parts)) {
+    stopifnot(
+      length(intersect(parts[[pp]], parts$reference)) == 0L,
+      length(parts[[pp]]) == fraction * n
+    )
+  }
+}
+
+
+for (fraction in c(0.01, 0.2, 0.5)) {
+  set.seed(0xBEEF)
+  parts <- sample_partitions_similar_weights_by_half(w, fraction = fraction, w_tolerance = 0.01)
+  str(parts)
+  idxs <- sort(unlist(parts, use.names = FALSE))
+  stopifnot(
+    names(parts)[1] == "reference",
+    length(parts) == 2L,
+    all(parts[[1]] == parts0[[1]]),
+    length(idxs) == (0.5 + fraction) * n
+  )
+  for (pp in 2:length(parts)) {
+    stopifnot(
+      length(intersect(parts[[pp]], parts$reference)) == 0L,
+      length(parts[[pp]]) == fraction * n
+    )
+  }
+}
+
