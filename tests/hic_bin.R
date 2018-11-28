649c669341ba30a741f4e6937c1ceac7a5c016bf
Henrik Bengtsson <hb@aroma-project.org>
2018-11-28 11:20:55 -0800

    CLEANUP: Drop remaining 'ref_vs_seq' usage

--- tests/hic_bin.R
+++ tests/hic_bin.R
@@ -1,16 +1,39 @@
 library(TopDomStudy)
 
+message("hic_bin() ...")
+
+message("- reading reads")
 pathname <- system.file("compiledData", "human,HAP1,unique,chr=22.rds", package = "TopDomStudy", mustWork = TRUE)
 reads <- read_rds(pathname)
 print(reads)
 
-binned <- hic_bin(reads, bin_size = 50000, intra_only = TRUE)
-str(binned)
+message("- binning")
+counts <- hic_bin(reads, bin_size = 50000, intra_only = TRUE)
+str(counts)
 stopifnot(
-  length(binned) == 1L,
-  names(binned) == "22",
-  is.matrix(binned[[1]]),
-  is.integer(binned[[1]]),
-  is.list(attr(binned[[1]], "bins"))
+  length(counts) == 1L,
+  names(counts) == "22",
+  is.matrix(counts[[1]]),
+  is.integer(counts[[1]]),
+  is.list(attr(counts[[1]], "bins"))
 )
 
+message("- Coercing to list of TopDomData objects")
+counts <- as_TopDomData(counts)
+print(counts)
+stopifnot(
+  length(counts) == 1L,
+  names(counts) == "22",
+  inherits(counts[[1]], "TopDomData")
+)
+
+message("- Fit TomDom for each TopDomData object")
+fits <- lapply(counts, FUN = TopDom, window.size = 5L)
+print(fits)
+stopifnot(
+  length(fits) == 1L,
+  names(fits) == "22",
+  inherits(fits[[1]], "TopDom")
+)
+
+message("hic_bin() ... done")
