a6e3746b1da2fd2279d866a6497bb3d2e8c4388b
Henrik Bengtsson <hb@aroma-project.org>
2020-01-17 11:05:20 -0800

    Forgot to add files

---
+++ R/read_overlap_regions.R
@@ -0,0 +1,44 @@
+#' Read TopDom Regions from Overlap Score Data
+#'
+#' @param pathname (character string) A \file{*,topdom.rds} file.
+#'
+#' @param format (character string) The returned format.
+#'
+#' @return A data frame with one TopDom domain per row.
+#'
+#' @importFrom utils file_test
+#' @importFrom tibble as_tibble
+#' @export
+read_topdom_regions <- function(pathname, format = c("tibble", "data.frame")) {
+  format <- match.arg(format)
+  stopifnot(file_test("-f", pathname))
+
+  fraction <- gsub(".*,fraction=([0-9.]+),.*", "\\1", basename(pathname))
+  stopifnot(nzchar(fraction))
+  fraction <- as.numeric(fraction)
+  stopifnot(is.numeric(fraction), length(fraction) == 1L, is.finite(fraction),
+            fraction > 0, fraction <= 1/2)
+  
+  seed <- gsub(".*,seed=([a-z0-9]+),.*", "\\1", basename(pathname))
+  stopifnot(nzchar(seed))
+  seed <- eval(parse(text = sprintf("0x%s", seed)))
+  stopifnot(is.numeric(seed), length(seed) == 1L, is.finite(seed))
+  
+  data <- readRDS(pathname)
+  
+  config <- attributes(data)[c("bin_size", "min_cell_size", "window_size", "partition_by", "seed")]
+  config$seed <- seed
+  config$fraction <- fraction
+  config <- config[c("bin_size", "fraction", "min_cell_size", "window_size", "partition_by", "seed")]
+
+  topdom <- data[["fraction=0.5"]]
+  td <- topdom$domain
+  data <- cbind(td, config)
+  stopifnot(is.data.frame(data), nrow(data) == nrow(td))
+  
+  if (format == "tibble") {
+    data <- as_tibble(data)
+  }
+  
+  data
+}
