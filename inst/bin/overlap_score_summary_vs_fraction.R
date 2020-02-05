c136e3f5dfbefdc2a1fe1eac664db28a4b058652
hb <hb@aroma-project.org>
2020-02-05 13:34:56 -0800

    Use a finer grid of (bin_size, rho):s

--- inst/bin/overlap_score_summary_vs_fraction.R
+++ inst/bin/overlap_score_summary_vs_fraction.R
@@ -30,11 +30,12 @@ options(future.globals.maxSize = 3 * 1024^3)
 
 bin_sizes <- c(5, 6, 8, 10, 12, 15, 20, 40, 60, 80, 100) * 1e3
 bin_sizes <- c(         10,         20, 40, 60, 80, 100) * 1e3
-bin_sizes <- c(   6, 8, 10, 12, 15, 20, 40, 60, 80, 100) * 1e3
+bin_sizes <- c(   6, 8, 10, 12, 15, 20, 30, 40, 50, 60, 80, 100) * 1e3
 ## FIXME: chromosome = "22", rho = 0.01, bin_size = 10000, nsamples = 1L
 ## gives an error
 rhos <- c(0.01, 0.02, 0.04, 0.05, 0.06, 0.08, 0.10, 0.20, 0.30, 0.40, 0.50)[-1]
 rhos <- c(      0.02, 0.04, 0.05, 0.06, 0.08, 0.10, 0.20, 0.30, 0.40, 0.50) ## Skip rho=0.02 due to chr=12 memory constraints
+rhos <- c(      0.02, 0.03, 0.04, 0.05, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.25, 0.30, 0.40, 0.50) ## Skip rho=0.02 due to chr=12 memory constraints
 
 domain_length <- NULL
 #domain_length <- c(300e3, 1000e3)
